9a895ece7045df432bab8538b148fdc741809d8b
Ricardo de la Rosa <ricardo@mitmiapp.com>
2015-04-28 21:23:21 +0200

    PHP added

--- concurso.php
+++ concurso.php
@@ -3,11 +3,36 @@
 	<title>concurso</title>
 </head>
 <body>
+	<script type="text/javascript">
+	alert("Informacion enviada");
+	</script>
 
 	<?php
+	$myFile = "concursantes/informacionConcursantes.txt";
+	$fh = fopen($myFile, 'a');
+
+	$dni =$_POST["DNI"];
+	$nombreYapellido=$_POST["nombre"];
+	$direccion =$_POST["direccion"];
+	$codigoPostal =$_POST["codigoPostal"];
+	$email =$_POST["email"];
+	$telefono =$_POST["telefono"];
+	$preferencia =$_POST["preferencia"];
+
+
 	
-	//echo $_POST["NIF"];
-	fwrite("informacionConcursantes.txt",$_POST["NIF"])
+	fwrite($fh,"\n" . "DNI: " . $dni . "\n");
+	fwrite($fh,"Nombre y apellidos: "  . $nombreYapellido . "\n");
+	fwrite($fh,"direccion "  . $direccion . "\n");
+	fwrite($fh,"codigoPostal: "  . $codigoPostal . "\n");
+	fwrite($fh,"email: "  . $email . "\n");
+	fwrite($fh,"telefono: "  . $telefono . "\n");
+	fwrite($fh,"preferencia: "  . $preferencia . "\n");
+	fclose($fh);
+
+
+	 header ("Location: inscrito.html");  
+
 	
 	?>
 
